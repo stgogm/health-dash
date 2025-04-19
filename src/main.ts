5dc804adfe653e69d72982d60bf2cfc2e86b660e
Santiago <momo@fiquu.dev>
2025-04-19 12:35:57 +0000

    feat: updated libs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core'
-import type { NestExpressApplication } from '@nestjs/platform-express'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const app = await NestFactory.create(AppModule)
   await app.listen(process.env.PORT || 3000)
 }
 
