4d9fcba66d73cb68dc1438ee0d9378bf8a61320f
Santiago <momo@fiquu.dev>
2025-04-20 17:53:53 +0000

    feat: add postgres conn

--- platform/api/src/app.module.ts
+++ platform/api/src/app.module.ts
@@ -1,4 +1,24 @@
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { Module } from '@nestjs/common'
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        port: parseInt(config.get('DATABASE_PORT') || '5432'),
+        username: config.get('DATABASE_USER'),
+        password: config.get('DATABASE_PASS'),
+        database: config.get('DATABASE_NAME'),
+        host: config.get('DATABASE_HOST'),
+        synchronize: true,
+        type: 'postgres',
+        // entities: [...],
+      }),
+    }),
+  ],
+})
 export class AppModule {}
